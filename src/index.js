bbdd3a22057adab1cd6735eee108768d2cf15071
CHASE <CHASE@Chases-MacBook-Pro.local>
2019-11-14 17:29:57 -0800

    (complete) redux-thunk integration & boilerplate setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import history from './history';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './App';
 import About from './components/about/About';
 import NotFound from './components/404/NotFound';
 import Events from './components/events/Events';
 import Details from './components/details/Details';
+
+import allReducer from './reducers';
+
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(reduxThunk)
+);
+
+const store = createStore(allReducer, composedEnhancers);
 
 // static navigation menu
 import Header from './components/Header';
@@ -24,6 +37,12 @@ const routing = (
     </Switch>
   </Router>
 );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
 
 ReactDOM.render(routing, document.getElementById('root'));
 
