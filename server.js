65bfe856100e89b290da23019f8a92881dfbfd40
CHASE <CHASE@Chases-MacBook-Pro.local>
2019-11-15 12:36:30 -0800

    (progress) initial docker setup

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
-const { PORT } = process.env;
+const { PORT = 3000 } = process.env;
+const HOST = '0.0.0.0';
 
 const app = express();
 
-app.use(express.static("build"));
+app.use(express.static('build'));
 
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "build", "index.html"))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 );
 
-app.listen(PORT, err => console.warn(err || `Listening on ${PORT}`));
+app.get('/', (req, res) => {
+  res.send('TESTING123');
+});
+
+app.listen(PORT, HOST, err => console.warn(err || `Listening on ${PORT}`));
