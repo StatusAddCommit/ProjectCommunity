b04a54f189948f42300afaaeee0b7b535c7fd5bc
CyanBlueCode <CHASE@Chases-MacBook-Pro.local>
2019-11-20 15:00:35 -0800

    (progress) converted about.html to react components

---
+++ src/components/about/AboutHeader.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import AboutNavMenu from './AboutNavMenu';
+import AboutSearchHeader from './AboutSearchHeader';
+
+class AboutHeader extends React.Component {
+  render() {
+    return (
+      <header
+        id="header"
+        className="header"
+        itemScope
+        itemType="http://schema.org/WPHeader"
+      >
+        {}
+        <nav
+          className="navigation-bar"
+          data-become-sticky={600}
+          data-no-placeholder
+        >
+          <div className="container">
+            <div className="hamburger">
+              <a href="#" />
+            </div>
+            <div className="navigation-bar-flex">
+              {}
+              <div className="logo">
+                <a href="index-2.html">
+                  {}
+                  Ummah
+                  <span>Charity</span>
+                </a>
+              </div>
+              {}
+              {}
+              <AboutNavMenu></AboutNavMenu>
+              {}
+              {}
+              <AboutSearchHeader></AboutSearchHeader>
+              {}
+              {}
+            </div>
+          </div>
+        </nav>
+        {}
+      </header>
+    );
+  }
+}
+
+export default AboutHeader;
