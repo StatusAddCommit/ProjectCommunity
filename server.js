20aed0aef80a7f469ab38be2b8fa3e076dc33697
Shams Ali <shams.ali0216@gmail.com>
2019-11-24 22:46:56 -0800

    (fix) package.json

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const proxy = require("express-http-proxy");
-const url = require("url");
-const path = require("path");
+const express = require('express');
+const proxy = require('express-http-proxy');
+const url = require('url');
+const path = require('path');
 
 const { PORT, API_URL } = process.env;
 
 const app = express();
 
-app.use(express.static("build"));
+app.use(express.static('build'));
 
 app.use(
-  "/umma-server",
+  '/umma-server',
   proxy(API_URL, {
     proxyReqPathResolver: req => `/umma-server${url.parse(req.url).path}`
   })
 );
 
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "build", "index.html"))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 );
 
 app.listen(PORT, err => console.warn(err || `Listening on ${PORT}`));
