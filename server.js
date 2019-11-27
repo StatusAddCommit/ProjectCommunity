98bd2fb473072ef7260c33f78746c1f4845e3707
CyanBlueCode <CHASE@Chases-MacBook-Pro.local>
2019-11-27 15:48:25 -0800

    (update) regenerate yarn.lock file

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 require('dotenv').config();
 
 const express = require('express');
+const proxy = require('express-http-proxy');
+const url = require('url');
 const path = require('path');
 
-const { PORT = 3000, HOST } = process.env;
+const { PORT, API_URL } = process.env;
 
 const app = express();
 
 app.use(express.static('build'));
 
+app.use(
+  '/umma-server',
+  proxy(API_URL, {
+    proxyReqPathResolver: req => `/umma-server${url.parse(req.url).path}`
+  })
+);
+
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 );
 
-app.listen(PORT, HOST, err => console.warn(err || `Listening on ${PORT}`));
+app.listen(PORT, err => console.warn(err || `Listening on ${PORT}`));
