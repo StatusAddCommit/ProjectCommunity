2f067f8aa59f11f2d48096ec432e56d3c262be12
CyanBlueCode <CHASE@Chases-MacBook-Pro.local>
2019-11-27 15:08:00 -0800

    (update) rebase with latest proxy-setup branch

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
-import history from './history';
-import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+import * as serviceWorker from './serviceWorker';
 
 import allReducer from './reducers';
 
@@ -34,6 +34,7 @@ import About from './components/about/About';
 import NotFound from './components/404/NotFound';
 import Events from './components/events/Events';
 import Details from './components/details/Details';
+import Header from './components/Header';
 
 import allReducer from './reducers';
 
@@ -44,27 +45,20 @@ const composedEnhancers = compose(
 
 const store = createStore(allReducer, composedEnhancers);
 
-// static navigation menu
-import Header from './components/Header';
-
 const routing = (
-  <Router>
-    <Header />
-    <Switch>
-      <Route path="/" exact component={App} />
-      <Route path="/about" component={About} />
-      <Route path="/events" component={Events} />
-      <Route path="/event:id" component={Details} />
-      <Route component={NotFound} />
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/events" component={Events} />
+        <Route path="/event:id" component={Details} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  </Provider>
 );
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('root')
 
 ReactDOM.render(routing, document.getElementById('root'));
 
