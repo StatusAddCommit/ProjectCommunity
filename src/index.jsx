7b15b6fa0f602cde7f0432f5414553b1ed9484ad
CHASE <chaseyang33@gmail.com>
2019-12-09 21:26:45 -0800

    (fix) Code cleanup (#15)
    * (progress) connect live data to event.jsx, project.jsx; remove reference html
    
    * (progress) connect main page projects & events with live data
    
    * (progress) connect eventsBanner; begin event details components
    
    * (progress) connect live data to event details page
    
    * (bug) detailsInfoBox google maps issue
    
    * (progress) replace remaining static text; add new constants props
    
    * (fix) initial fix for progress bar & map in project.jsx
    
    * (fix) flip map in event details page
    
    * (fix) replace unsupported owl-carousel with new slick-carousel
    
    * (complete) replace all static data with live data
    
    * (progress) fix hamburger menu click action, but does not close after click
    
    * (progress) add conditional rendering for AboutSectionTeam
    
    * (complete) mobile hamburger navmenu bug fix
    
    * (complete) google form submit function setup and working
    
    * (progress) add required fields message render for contact form
    
    * (progress) add conditional rendering for timeline
    
    * (progress) add view all events button to home page sectionevents
    
    * (fix) bug that potentially renders wrong event details page
    
    * (complete) general code cleanup
    
    * (fix) remove unnecessary files
    
    * (fix) remove ReduxDevTools
    
    * (fix) remove ReduxDevTools for prod

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import allReducers from './reducers';
@@ -16,14 +16,7 @@ import Events from './components/events/Events';
 import Details from './components/details/Details';
 import Header from './components/Header';
 
-const { __REDUX_DEVTOOLS_EXTENSION__: REDUX_DEVTOOLS } = window;
-
-const composedEnhancers = compose(
-  applyMiddleware(thunk),
-  REDUX_DEVTOOLS && REDUX_DEVTOOLS()
-);
-
-const store = createStore(allReducers, composedEnhancers);
+const store = createStore(allReducers, applyMiddleware(thunk));
 
 const routing = (
   <Provider store={store}>
